a445de07e424c96303f74e3669ee927f481a39f0
vishal-uttamchandani <vishalu@gmail.com>
2018-09-19 19:21:29 -0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Download and install the moby-engine
+curl -L https://aka.ms/moby-engine-armhf-latest -o moby_engine.deb && sudo dpkg -i ./moby_engine.deb
+
+# Run apt-get fix
+sudo apt-get install -f
+
+# Download and install the standard libiothsm implementation
+curl -L https://aka.ms/libiothsm-std-linux-armhf-latest -o libiothsm-std.deb && sudo dpkg -i ./libiothsm-std.deb
+
+# Download and install the IoT Edge Security Daemon
+curl -L https://aka.ms/iotedged-linux-armhf-latest -o iotedge.deb && sudo dpkg -i ./iotedge.deb
+
+# Run apt-get fix
+sudo apt-get install -f
